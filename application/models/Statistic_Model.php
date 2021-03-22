7412a95c20714c946c79ecb885e2edd97b22b7c1
A1 <wahyuandika777@gmail.com>
2021-03-22 12:47:40 +0700

    Awal Upload

---
+++ application/models/Statistic_Model.php
@@ -0,0 +1,406 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Statistic_Model extends CI_Model {
+
+    function getTrafficDashboard($interface){
+        // $this->db->where('interface',$interface);
+        // $this->db->order_by('id', 'desc');
+        // $this->db->limit(10);
+        // $this->db->order_by('time', 'asc');
+        // // $this->db->limit(2000);
+        // $data = $this->db->get('network_log');
+        $data = $this->db->query( "SELECT * FROM (SELECT * FROM network_log WHERE interface = '".$interface."'
+        ORDER BY time DESC LIMIT 120 ) as ether order by id ASC "
+        );
+        return $data->result();
+    }
+    
+    function getDataInterface($interface){
+        $this->db->where('interface',$interface['interface']);
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        $data = $this->db->get('network_log');
+        return $data->result();
+    }
+
+    function getStatisticInterface($interface){
+        $this->db->select('max(tx) as MaxTx, min(tx) as MinTx, avg(tx) as AvgTx, max(rx) as MaxRx, min(rx) as MinRx, avg(rx) as AvgRx');
+        $this->db->where('interface',$interface['interface']);
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        $data = $this->db->get('network_log');
+        return $data->row_array();
+    }
+
+    function getQuality($interface){
+        $this->db->where('interface',$interface['interface']);
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        $data = $this->db->get('network_quality_log');
+        return $data->result();
+    }
+
+	function getPingInterfaceSeries($p){
+		$sql = "SELECT DISTINCT interface FROM network_quality_log WHERE time>='".$p['time']['start']."' AND time <= '".$p['time']['end']."' ORDER BY interface";
+		$db = $this->db->query($sql)->result_array();
+		
+		$res = array();
+		foreach($db as $row){
+			$res[] = array(
+				'name'=>$row['interface'].' Loss',
+				'data'=>array(),
+				'type'=>'column',
+				'yAxis' => 1,
+				'tooltip'=> array('valueSuffix' => ' %'),
+			);
+			$res[] = array(
+				'name'=>$row['interface'].' Ping Time',
+				'data'=>array(),
+				'type'=>'spline',
+				'yAxis' => 0,
+				'tooltip'=> array('valueSuffix' => ' ms')
+			);
+		}
+		return $res;
+	}
+	
+	function getPingData($p){
+		$interval = null;
+		// cari selisih tanggal
+		$diff = intval($this->__getDateDiff($p));
+		
+		// IF rentang antara 12-24 jam
+		if($diff >= 12*60 && $diff <= 24*60) $interval = 5*60; // 5 menit
+		// IF rentang antara 1-7 hari
+		else if($diff >= 24*60 && $diff <= 7*24*60) $interval = 30*60; // 30 menit
+		// IF rentang antara 7-30 hari
+		else if($diff >= 7*24*60 && $diff <= 30*24*60) $interval = 60*60; // 1 jam
+		// IF rentang antara 30-60 hari
+		else if($diff >= 30*24*60 && $diff <= 60*24*60) $interval = 2*60*60; // 2 jam
+		// IF rentang antara 60-90 hari
+		else if($diff >= 60*24*60 && $diff <= 90*24*60) $interval = 3*60*60; // 3 jam
+		else $interval = 6*60*60;
+		
+		// IF rentang LAST 3 HOURS
+		if(substr($p['time']['start'],-6)=='111111' && substr($p['time']['end'],-6)=='111111'){ // LAST 3 HOURS
+			if(substr($p['intf'],-9) == 'Ping Time')
+				return array(
+					'subtitle' => 'Last 3 hours - '.$this->getPingData_subLast3Hours(),
+					'data' => $this->getPingDataTime_Last3Hours($p),
+				);
+			else if(substr($p['intf'],-4) == 'Loss')
+				return array(
+					'subtitle' => 'Last 3 hours - '.$this->getPingData_subLast3Hours(),
+					'data' => $this->getPingDataLoss_Last3Hours($p),
+				);
+			else return false;	
+		}else // IF rentang LAST 6 HOURS
+		if(substr($p['time']['start'],-6)=='222222' && substr($p['time']['end'],-6)=='222222'){ // LAST 6 HOURS
+			if(substr($p['intf'],-9) == 'Ping Time')
+				return array(
+					'subtitle' => 'Last 6 hours - '.$this->getPingData_subLast6Hours(),
+					'data' => $this->getPingDataTime_Last6Hours($p),
+				);
+			else if(substr($p['intf'],-4) == 'Loss')
+				return array(
+					'subtitle' => 'Last 6 hours - '.$this->getPingData_subLast6Hours(),
+					'data' => $this->getPingDataLoss_Last6Hours($p),
+				);
+			else return false;	
+		}
+		else if(!empty($interval)){
+			if(substr($p['intf'],-9) == 'Ping Time')
+				return array(
+					'subtitle' => $this->getPingData_sub($p),
+					'data' => $this->getPingDataTime_Interval($p,$interval),
+				);
+			else if(substr($p['intf'],-4) == 'Loss')
+				return array(
+					'subtitle' => $this->getPingData_sub($p),
+					'data' => $this->getPingDataLoss_Interval($p,$interval),
+				);
+			else return false;
+		}else{
+			if(substr($p['intf'],-9) == 'Ping Time')
+				return array(
+					'subtitle' => $this->getPingData_sub($p),
+					'data' => $this->getPingDataTime($p),
+				);
+			else if(substr($p['intf'],-4) == 'Loss')
+				return array(
+					'subtitle' => $this->getPingData_sub($p),
+					'data' => $this->getPingDataLoss($p),
+				);
+			else return false;
+		}
+	}
+	
+	function getPingDataTime($p){
+		//CASE 
+		//	WHEN loss > 75 THEN '#ff0000'
+		//	ELSE '#00ff00'
+		//END AS `color`
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x', 
+				-- DATE_FORMAT(time,'%Y-%m-%d %H:%i:00') AS 'x', 
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				ping_avg AS `y`
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-9)."' 
+				AND time>='".$p['time']['start']."' 
+				AND time <= '".$p['time']['end']."' ";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataTime_Last3Hours($p){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x', 
+				-- DATE_FORMAT(time,'%Y-%m-%d %H:%i:00') AS 'x', 
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				ping_avg AS `y`
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-9)."' 
+				AND time>=DATE_ADD(NOW(), INTERVAL - 3 HOUR)";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataTime_Last6Hours($p){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x', 
+				-- DATE_FORMAT(time,'%Y-%m-%d %H:%i:00') AS 'x', 
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				ping_avg AS `y`
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-9)."' 
+				AND time>=DATE_ADD(NOW(), INTERVAL - 6 HOUR)";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataTime_Interval($p,$interval){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x', 
+				DATE_FORMAT(AVG(time),'%Y-%m-%d %H:%i') AS `name`,
+				AVG(ping_avg)  AS `y`
+			FROM 
+				network_quality_log
+			WHERE
+				interface = '".substr($p['intf'],0,-9)."' 
+				AND time>='".$p['time']['start']."' 
+				AND time <= '".$p['time']['end']."'
+			GROUP BY
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) DIV ".$interval;
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataLoss($p){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x', 
+				-- DATE_FORMAT(time,'%Y-%m-%d %H:%i:00') AS 'x',
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				loss AS `y`
+				
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-4)."' 
+				AND time>='".$p['time']['start']."' 
+				AND time <= '".$p['time']['end']."' ";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataLoss_Last3Hours($p){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x',
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				loss AS `y`
+				
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-4)."' 
+				AND time >= DATE_ADD(NOW(), INTERVAL - 3 HOUR)";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataLoss_Last6Hours($p){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x',
+				DATE_FORMAT(time,'%Y-%m-%d %H:%i') AS `name`,
+				loss AS `y`
+				
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-4)."' 
+				AND time >= DATE_ADD(NOW(), INTERVAL - 6 HOUR)";
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+	function getPingDataLoss_Interval($p,$interval){
+		$sql = "
+			SELECT
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) * 1000 AS 'x',
+				DATE_FORMAT(AVG(time),'%Y-%m-%d %H:%i') AS `name`,
+				AVG(loss) AS `y`
+			FROM 
+				network_quality_log 
+			WHERE 
+				interface = '".substr($p['intf'],0,-4)."' 
+				AND time>='".$p['time']['start']."' 
+				AND time <= '".$p['time']['end']."' 
+			GROUP BY
+				UNIX_TIMESTAMP(DATE_FORMAT(time,'%Y-%m-%d %H:%i:00')) DIV ".$interval;
+		$_db = $this->db->query($sql)->result_array();
+		$res = array();
+		foreach($_db as $_row){
+			//$res[] = array($_row['y'],intval($_row['x']));
+			$_row['x'] = intval($_row['x']);
+			$_row['y'] = intval($_row['y']);
+			$res[] = $_row;
+		}
+		return $res;
+	}
+
+	function getPingData_subLast3Hours(){
+		$sql = "SELECT
+		CONCAT(
+			DATE_FORMAT(
+				DATE_ADD(NOW(), INTERVAL - 3 HOUR),
+				'%d %M %Y %H:%i:%s'
+			),
+			' s/d ',
+			DATE_FORMAT(NOW(), '%d %M %Y %H:%i:%s')
+		) AS `subtitle`";
+		$res = $this->db->query($sql)->row_array();
+		return $res['subtitle'];
+	}
+	function getPingData_subLast6Hours(){
+		$sql = "SELECT
+		CONCAT(
+			DATE_FORMAT(
+				DATE_ADD(NOW(), INTERVAL - 6 HOUR),
+				'%d %M %Y %H:%i:%s'
+			),
+			' s/d ',
+			DATE_FORMAT(NOW(), '%d %M %Y %H:%i:%s')
+		) AS `subtitle`";
+		$res = $this->db->query($sql)->row_array();
+		return $res['subtitle'];
+	}
+	function getPingData_sub($p){
+		$sql = "SELECT
+		CONCAT(
+			DATE_FORMAT(
+				'".$p['time']['start']."',
+				'%d %M %Y %H:%i:%s'
+			),
+			' s/d ',
+			DATE_FORMAT(
+				'".$p['time']['end']."',
+				'%d %M %Y %H:%i:%s'
+			)
+		) AS `subtitle`";
+		$res = $this->db->query($sql)->row_array();
+		return $res['subtitle'];
+	}
+	
+	function __getDateDiff($p){
+		$sql = "SELECT TIMESTAMPDIFF(MINUTE, '".$p['time']['start']."','".$p['time']['end']."') AS `diff`";
+		$res = $this->db->query($sql)->row_array();
+		return $res['diff'];
+	}
+	
+    function getStatisticQuality($interface){
+        $this->db->select('max(ping_avg) as MaxPing, min(ping_avg) as MinPing, avg(ping_avg) as AvgPing, max(jitter) as MaxJitter, min(jitter) as MinJitter, avg(jitter) as AvgJitter, max(loss) as MaxLoss, min(loss) as MinLoss, avg(loss) as AvgLoss');
+        $this->db->where('interface',$interface['interface']);
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        $data = $this->db->get('network_quality_log');
+        return $data->row_array();
+    }
+
+    function getDataResource($interface){
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        // $this->db->order_by('time', 'desc');
+        // $this->db->limit(2000);
+        // $this->db->limit(10);
+        $data = $this->db->get('resource_log');
+        return $data->result();
+    }
+
+    function getStatisticResource($interface){
+        $this->db->select('max(cpu) as MaxCPU, min(cpu) as MinCPU, avg(cpu) as AvgCPU, max((memory/memory_capacity)*100) as MaxMemory, min((memory/memory_capacity)*100) as MinMemory, avg((memory/memory_capacity)*100) as AvgMemory');
+        $this->db->where('time >=',$interface['first_date']);
+        $this->db->where('time <=',$interface['last_date']);
+        // $this->db->order_by('time', 'desc');
+        // $this->db->limit(2000);
+        // $this->db->limit(10);
+        $data = $this->db->get('resource_log');
+        return $data->row_array();
+    }
+}
+
+/* End of file Statistic_Model.php */
---
+++ application/views/statistic_ping_line_view.php
@@ -0,0 +1,260 @@
+<?php $this->load->view('Templates/headersidebar_view'); ?>
+<style>
+</style>
+</div>
+    </div>
+    <div class="static-content-wrapper">
+        <div class="static-content">
+            <div class="page-content">
+                <div class="container-fluid" style="margin-top: 10px">
+                    <!-- <div data-widget-group="group1"> -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading"> 
+										<h2>Statistic Ping Quality</h2>
+										<div class="col-md-3 pull-right">
+											<button class="btn btn-default" id="daterangepicker2">
+												<i class="ti ti-calendar"></i> 
+												<span></span> <b class="caret"></b>
+											</button>											
+										</div>
+										
+										<div class="btn-group pull-right" id="button-table" role="group" aria-label="Basic example">
+										<a type="button" class="btn btn-success" data-aksi="refresh" style="margin:10px 0 0 0px"><i class="fa fa-refresh"></i></a>
+											<a type="button" class="btn btn-success" data-aksi="print" style="margin:10px 0 0 0px"><i class="fa fa-print"></i></a>  
+										</div>
+                                    </div>
+                                    <div class="panel-body" id="divPrint">
+										<div class="row">
+                                            <div style="margin: 10px">
+                                                <h5><i class="fa fa-circle" style="color: #5cb85c"></i></h5>
+                                                <div class="mychartQuality" id="quality1" style="height: 600px;" class="mt-sm mb-sm" data-interface="Indosat"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <!-- </div> -->
+                </div> 
+                        <!-- </div>  -->
+                    <!-- </div> -->
+                <!-- <footer role="contentinfo">
+                    <div class="clearfix">
+                        <ul class="list-unstyled list-inline pull-left">
+                            <li><h6 style="margin: 0;">&copy; 2015 Avenxo</h6></li>
+                        </ul>
+                        <button class="pull-right btn btn-link btn-xs hidden-print" id="back-to-top"><i class="ti ti-arrow-up"></i></button>
+                    </div>
+                </footer> -->
+<?php $this->load->view('Templates/footer_view'); ?>
+
+
+<script type="text/javascript">
+	$(document).ready(function(){
+        $('.select-device').select2({width: '100%'});
+        $(".select-device").each(function() {
+            $(this).siblings(".select2-container").css('border', '1px solid #e3e3e3;');
+        });
+		
+		//drawchart();
+		$('#reportrange span').html(moment().subtract('days', 30).format('D MMMM YYYY') + ' - ' + moment().format('D MMMM YYYY'));
+    })
+	var startDate;
+	var endDate;
+
+	$('body').on('click','a[data-aksi="print"]',function(){
+        // $("#divPrint").show();  
+		javascript:window.print();
+		// printDiv()
+    });
+
+	function printDiv(){
+		var divToPrint=document.getElementById('divPrint');
+
+		var newWin=window.open('','Print-Window');
+
+		newWin.document.open();
+
+		newWin.document.write('<html><body onload="window.print()"><h2 style="text-align:center">Statistic</h2>'+divToPrint.innerHTML+'</body></html>');
+
+		newWin.document.close();
+
+		setTimeout(function(){newWin.close();},10);
+	}
+
+	Highcharts.setOptions({
+		global: {
+			timezoneOffset: -7 * 60
+		}
+	});
+
+	var mychart = null;
+	var intf_ser = [];
+	var def_vis_on = ['CBN2 Ping Time','CBN2 Loss','Indosat Ping Time'];
+	var rep_time = [];
+	
+	function drawchart(){
+		$.post('<?php echo site_url("Statistic/getPingInterface");?>',{time:rep_time},function(data){
+			intf_ser = data;
+			$.each(data,function(i,v){
+				data[i].events = {
+					legendItemClick : function(e){
+						getchartdata(i, true);
+					}
+				}
+			});
+			mychart = Highcharts.chart('quality1', {
+				chart: {
+					zoomType: 'xy'
+				},
+				title: {
+					text: 'Ping & Loss Quality Statictic'
+				},
+				subtitle: {
+					text: 'Processsing... please wait'
+				},
+				xAxis: [{
+					crosshair: true
+				}],
+				yAxis: [{ // Primary yAxis
+					labels: {
+						format: '{value} ms',
+						style: {
+							color: Highcharts.getOptions().colors[1]
+						}
+					},
+					title: {
+						text: 'Ping Time',
+						style: {
+							color: Highcharts.getOptions().colors[1]
+						}
+					},
+					min : -2,
+				}, { // Secondary yAxis
+					title: {
+						text: 'Loss',
+						style: {
+							color: Highcharts.getOptions().colors[0]
+						}
+					},
+					labels: {
+						format: '{value}',
+						style: {
+							color: Highcharts.getOptions().colors[0]
+						}
+					},
+					min : -2,
+					max : 100,
+					opposite: true,
+				}],
+				xAxis: {
+					type : 'datetime',
+				},
+				tooltip: {
+					shared: true
+				},
+				legend: {
+					layout: 'vertical',
+					align: 'left',
+					verticalAlign: 'middle',
+					backgroundColor:
+						Highcharts.defaultOptions.legend.backgroundColor || // theme
+						'rgba(255,255,255,0.25)'
+				},
+				series: data,
+				responsive: {
+					rules: [{
+						condition: {
+							
+						},
+						chartOptions: {
+							legend: {
+								layout: 'horizontal',
+								align: 'center',
+								verticalAlign: 'bottom'
+							}
+						}
+					}]
+				}
+			});
+			
+			
+			$.each(intf_ser,function(j,intf){
+				mychart.series[j].hide();
+			})
+			$.each(def_vis_on,function(i,v){
+				$.each(intf_ser,function(j,intf){
+					if(intf['name']==v){
+						mychart.series[j].show();
+					}
+				})
+			})
+			getchartdata();
+		},'json');
+	}
+	var clsTime;
+	function getchartdata(intf_i = 0,ignoreVisible = false){
+		if(!mychart.series[intf_i].visible && !ignoreVisible){
+			if(typeof intf_ser[intf_i+1] != 'undefined') getchartdata(intf_i+1);
+			else{
+				clearTimeout(clsTime);
+				clsTime = setTimeout(function(){ getchartdata(); }, 1000 * 60);
+			}
+			return false;
+		}
+		$.post('<?php echo site_url("Statistic/getPingData");?>',{intf:intf_ser[intf_i]['name'],time:rep_time},function(data){
+			var isvis = mychart.series[intf_i].visible;
+			mychart.series[intf_i].update({data:data.data});
+			if(isvis){
+				mychart.series[intf_i].show();
+			}else {
+				mychart.series[intf_i].hide();
+			}
+			mychart.subtitle.update({text:data.subtitle});
+			if(typeof intf_ser[intf_i+1] != 'undefined') getchartdata(intf_i+1);
+			else{
+				clearTimeout(clsTime);
+				clsTime = setTimeout(function(){ getchartdata(); }, 1000 * 60);
+			}
+		},'json');
+	}
+	
+	var last3hour = [moment().subtract('hours', 3).millisecond(111111),moment().millisecond(111111)];
+	var last6hour = [moment().subtract('hours', 6).millisecond(222222),moment().millisecond(222222)];
+	$('#daterangepicker2').daterangepicker({
+		timePicker: true,
+		timePickerIncrement: 5,
+		use24hours: true,
+		ranges: {
+			'Last 3 hours': last3hour,
+			'Last 6 hours': last6hour,
+			'Today': [moment().startOf('day'), moment()],
+			'Yesterday': [moment().subtract('days', 1).startOf('day'), moment().subtract('days', 1).endOf('day')],
+			'Last 7 Days': [moment().subtract('days', 6).startOf('day'), moment()],
+			'Last 30 Days': [moment().subtract('days', 29).startOf('day'), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			// 'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+		},
+		opens: 'left',
+		startDate: last6hour[0],
+		endDate: last6hour[1]
+	},
+
+	function(start, end) {
+		var date = {start: start.format('YYYY-MM-DD HH:mm:ss SSSSSS'),
+			end: end.format('YYYY-MM-D HH:mm:ss SSSSSS'),
+		};
+		$('#daterangepicker2 span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+		
+		startDate = start;
+		endDate = end;
+		
+		rep_time = date;
+		drawchart();
+	});
+</script>
+
+</body>
+</html>
